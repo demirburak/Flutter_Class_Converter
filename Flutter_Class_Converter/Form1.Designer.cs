0064d455a25634fef8b6cfb6283febecca0522dd
Burak Demiraslan <demirburak@gmail.com>
2022-07-11 14:29:05 +0300

    Add project files.

---
+++ Flutter_Class_Converter/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace Flutter_Class_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnYardim = new System.Windows.Forms.Button();
+            this.btnDonustur = new System.Windows.Forms.Button();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.PROPERTIES = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtMetin = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnTemizle);
+            this.panel1.Controls.Add(this.btnYardim);
+            this.panel1.Controls.Add(this.btnDonustur);
+            this.panel1.Controls.Add(this.txtModel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 94);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "C# Model Name";
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(516, 31);
+            this.btnTemizle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(148, 22);
+            this.btnTemizle.TabIndex = 3;
+            this.btnTemizle.Text = "Clean";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnYardim
+            // 
+            this.btnYardim.Location = new System.Drawing.Point(680, 30);
+            this.btnYardim.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnYardim.Name = "btnYardim";
+            this.btnYardim.Size = new System.Drawing.Size(64, 22);
+            this.btnYardim.TabIndex = 2;
+            this.btnYardim.Text = "How To";
+            this.btnYardim.UseVisualStyleBackColor = true;
+            this.btnYardim.Click += new System.EventHandler(this.btnYardim_Click);
+            // 
+            // btnDonustur
+            // 
+            this.btnDonustur.Location = new System.Drawing.Point(363, 31);
+            this.btnDonustur.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDonustur.Name = "btnDonustur";
+            this.btnDonustur.Size = new System.Drawing.Size(148, 22);
+            this.btnDonustur.TabIndex = 1;
+            this.btnDonustur.Text = "Convert";
+            this.btnDonustur.UseVisualStyleBackColor = true;
+            this.btnDonustur.Click += new System.EventHandler(this.btnDonustur_Click);
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(11, 31);
+            this.txtModel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(336, 23);
+            this.txtModel.TabIndex = 0;
+            // 
+            // dgv
+            // 
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PROPERTIES});
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dgv.Location = new System.Drawing.Point(0, 94);
+            this.dgv.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 29;
+            this.dgv.Size = new System.Drawing.Size(360, 302);
+            this.dgv.TabIndex = 1;
+            this.dgv.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dgv_KeyDown);
+            // 
+            // PROPERTIES
+            // 
+            this.PROPERTIES.HeaderText = "PROPERTIES";
+            this.PROPERTIES.MinimumWidth = 6;
+            this.PROPERTIES.Name = "PROPERTIES";
+            // 
+            // txtMetin
+            // 
+            this.txtMetin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMetin.Location = new System.Drawing.Point(360, 94);
+            this.txtMetin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMetin.Multiline = true;
+            this.txtMetin.Name = "txtMetin";
+            this.txtMetin.Size = new System.Drawing.Size(396, 302);
+            this.txtMetin.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 396);
+            this.Controls.Add(this.txtMetin);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Converter C# Model To Flutter/Dart Class";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnYardim;
+        private System.Windows.Forms.Button btnDonustur;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox txtMetin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PROPERTIES;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTemizle;
+    }
+}
+
---
+++ Flutter_Class_Converter/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flutter_Class_Converter
+{
+    public partial class Form1 : Form
+    {
+        bool JsonParametresiEklensinmi = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYardim_Click(object sender, EventArgs e)
+        {
+            string bilgi = "(EN) You can copy/paste properties from c# model (class) to table below. Write to model (class) name and click convert button.\n\n";
+            bilgi += "(TR) Sadece class taki propertyleri aşağıdaki tabloya yapıştır. Model in isminide textbox a  yaz ve dönüştür tıkla.\n\n";
+            bilgi += "Burak D.";
+            MessageBox.Show(bilgi);
+        }
+
+        private void dgv_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                string s = Clipboard.GetText();
+                string[] lines = s.Split('\n');
+                int row = dgv.CurrentCell.RowIndex;
+                int col = dgv.CurrentCell.ColumnIndex;
+                foreach (string line in lines)
+                {
+                    if (row < dgv.RowCount && line.Length > 0)
+                    {
+                        if (dgv.Rows.Count - 1 == row) dgv.Rows.Add();
+
+                        string[] cells = line.Split('\t');
+                        for (int i = 0; i < cells.GetLength(0); ++i)
+                        {
+                            if (col + i < this.dgv.ColumnCount)
+                            {
+                                dgv[col + i, row].Value = (Convert.ChangeType(cells[i], dgv[col + i, row].ValueType)).ToString().Replace("\n", "").Replace("\r", "").Replace("\t", "").Replace((char)160, (char)32);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        row++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        private void btnDonustur_Click(object sender, EventArgs e)
+        {
+            List<string> isimler = new List<string>();
+            string model = txtModel.Text;
+            string metin = "part '" + model.ToLower() + ".g.dart'; \n\n";
+
+            metin += "@JsonSerializable()\n";
+            metin += "class " + model + " { \n\n";
+
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                
+                kelimeDonustur(row.Cells[0].Value, out string isim, out string ozellik);
+                if(!string.IsNullOrEmpty(isim)&& !string.IsNullOrEmpty(isim))
+                {
+                    isimler.Add(isim);
+
+                    metin += ozellik + " " + isim + ";\n";
+                }
+            }
+
+            metin += "\n\n";
+
+            metin += model + "({ ";
+            foreach (var isim in isimler)
+            {
+                metin += "this." + isim + " ,\n";
+            }
+            metin = metin.Substring(0, metin.Length - 2) + "}); \n\n";
+
+            metin += "factory " + model + ".fromJson(Map<String, dynamic> jsonData) => " +
+                " _$" + model + "FromJson(jsonData);\n\n";
+
+            metin += "Map<String, dynamic> toJson() => _$"+model+"ToJson(this);\n\n";
+
+            metin += model + ".clone(" + model + " source): \n";
+            foreach (var isim in isimler)
+            {
+                metin += "this." + isim + " = source." + isim + " ,\n";
+            }
+            metin = metin.Substring(0, metin.Length - 2) + ";";
+
+            metin += " } \n";
+
+            //json parametresi
+            if (JsonParametresiEklensinmi)
+            {
+                metin = metin.Replace("@JsonSerializable()", "@JsonSerializable(explicitToJson: true)");
+            }
+
+            txtMetin.Text = metin;
+
+
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txtModel.Text = string.Empty;
+            txtMetin.Text = string.Empty;
+            JsonParametresiEklensinmi = false;
+            dgv.Rows.Clear();
+        }
+
+        private void kelimeDonustur(object value, out string isim, out string ozellik)
+        {
+            isim = string.Empty;
+            ozellik = string.Empty;
+            if (value != null)
+            {
+                try
+                {
+                    string satir = value.ToString();
+                    satir = satir.Replace("public", "");
+                    string[] temp = satir.Split(" ".ToCharArray());
+                    string[] bilgiler = temp.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+                    if (bilgiler.Length > 2)
+                    {
+                        ozellik = bilgiler[0];
+                        isim = bilgiler[1];
+                    }
+
+                    if(!string.IsNullOrEmpty(ozellik) && !string.IsNullOrEmpty(isim))                        
+                    {
+                        ozellik = ozellikDonusum(ozellik);
+                        isim = isimDonusum(isim);
+                    }
+
+                }
+                catch (Exception s)
+                {
+
+                }
+            }
+        }
+
+        private string isimDonusum(string isim)
+        {
+            isim = isim.Replace(" ", "");
+            int usindex = isim.IndexOf("_");
+            if (usindex == -1 || usindex == 0)
+                isim = isim.ToLower();
+            else
+            {
+                if (usindex == 1)
+                {
+                    string ilk = isim.Substring(0, 1);
+                    string son = isim.Replace(ilk, "");
+                    isim = ilk.ToLower() + son;
+                }
+                else
+                {
+                    string ilk = isim.Substring(0, usindex - 1);
+                    string son = isim.Replace(ilk, "");
+                    isim = ilk.ToLower() + son;
+                }
+            }
+
+            isim = isim.Replace("ı", "i").Replace("İ", "I");
+
+
+            return isim;
+        }
+
+        private string ozellikDonusum(string ozellik)
+        {
+            ozellik = ozellik.Replace(" ", "");
+            switch (ozellik)
+            {
+                case "int":
+                    ozellik = "int";
+                    break;
+                case "double":
+                    ozellik = "double";
+                    break;
+                case "string":
+                    ozellik = "String";
+                    break;
+                case "decimal":
+                    ozellik = "double";
+                    break;
+                case "bool":
+                    ozellik = "bool";
+                    break;
+                case "DateTime":
+                    ozellik = "DateTime";
+                    break;
+                default:
+                    {
+                        List<string> anaOzellikler = new List<string>() { "int",
+                        "double","decimal","string","bool","DateTime"};
+
+                        if (ozellik.IndexOf("<") > -1)
+                        {
+                            string[] temp1 = ozellik.Split("<".ToCharArray());
+                            string birinciOzellik = temp1[0];
+                            string isim = ozellik.Replace(birinciOzellik, "").Replace("<", "").Replace(">", "");
+                            if(!anaOzellikler.Contains(isim) && !JsonParametresiEklensinmi)
+                            {
+                                JsonParametresiEklensinmi = true;
+                            }
+
+                            //ozellik = birinciOzellik + "<" + isimDonusum(isim) + ">";
+                        }
+                        else
+                        {
+                            if (!anaOzellikler.Contains(ozellik) && !JsonParametresiEklensinmi)
+                            {
+                                JsonParametresiEklensinmi = true;
+                            }
+                        }
+
+                    }
+                    break;
+            }
+
+            return ozellik;
+        }
+    }
+}
